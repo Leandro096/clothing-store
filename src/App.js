ab549ee394d9e2864d6f55c3c65bb8024dba04bb
Leandro J. Garcia <67251206+Leandro096@users.noreply.github.com>
2024-03-14 13:35:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,14 +19,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <div className="app">
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
